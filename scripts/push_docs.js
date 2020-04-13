e55b380c434e5ff87b02b58fbd0a1ade79c8fe84
zhangyuheng(vczyh) <vczyh@foxmail.com>
2020-04-14 02:35:58 +0800

    auto

--- scripts/push_docs.js
+++ scripts/push_docs.js
@@ -2,7 +2,7 @@ const shell = require('shelljs')
 
 shell.cd('docs')
 shell.exec(`git init`)
-shell.exec(`git remote add origin https://github.com/vczyh/docs.git`)
+shell.exec(`git remote add origin git@github.com:vczyh/docs.git`)
 shell.exec(`git add -A`)
 shell.exec(`git commit -m auto`)
 shell.exec(`git push -u origin master:blog`)
