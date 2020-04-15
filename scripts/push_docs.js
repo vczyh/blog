7c2d757941300a2b6972de47095824b25c36fbc3
zhangyuheng(vczyh) <vczyh@foxmail.com>
2020-04-15 23:38:42 +0800

    auto

--- scripts/push_docs.js
+++ scripts/push_docs.js
@@ -1,8 +1,8 @@
 const shell = require('shelljs')
 
-shell.cd('docs')
-shell.exec(`git init`)
-shell.exec(`git remote add origin git@github.com:vczyh/docs.git`)
+// shell.cd('docs')
+// shell.exec(`git init`)
+// shell.exec(`git remote add origin git@github.com:vczyh/docs.git`)
 shell.exec(`git add -A`)
 shell.exec(`git commit -m auto`)
 shell.exec(`git push -u origin master:vuepress-something`)
