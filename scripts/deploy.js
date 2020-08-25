8a4f816250cc9af5c3467e219ea251a25c967b29
vczyh <zhangeek@qq.com>
2020-08-25 20:17:46 +0800

    deploy

---
+++ scripts/deploy.js
@@ -0,0 +1,12 @@
+const shell = require(`shelljs`)
+
+shell.exec(`yarn build`)
+shell.exec(`cd public`)
+shell.exec(`echo "blog.zhangeek.com" > CNAME`)
+
+shell.exec(`git init`)
+shell.exec(`git add -A`)
+shell.exec(`git commit -m "deploy"`)
+
+// shell.exec(`git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master`)
+shell.exec(`git push -f git@e.coding.net:vczyh/blog/static.git blog:blog`)
