67b2c875b29acfa8132c5ed8b31b181e0d43a284
vczyh <zhangeek@qq.com>
2020-09-15 00:27:03 +0800

    update

--- posts/.vuepress/data/friendData.js
+++ posts/.vuepress/data/friendData.js
@@ -11,4 +11,16 @@ export default [
     link: 'https://zhangeek.com/',
     avatar: 'http://p.vczyh.com/blog/avatar.jpg'
   },
+  {
+    title: '野宁新之助',
+    desc: '说说我的生活',
+    link: 'https://www.sanghangning.cn',
+    avatar: 'https://cdn.chenyingshuang.cn/index/avatar.jpg'
+  },
+  {
+    title: 'YEVPT',
+    desc: '我喜欢要么极度悲伤要么淡淡温暖。',
+    link: 'https://www.yevpt.com',
+    avatar: 'https://tc.yevpt.com/images/2020/06/15/vpt-avatar-img-7.md.png'
+  },
 ]
